aaf9297f7dfd1c001b46b5b7639ce3104358043f
00-2 <9x02@mail.ru>
2021-09-28 04:16:12 +0300

    add parallax effect

--- Project/Prototype/guido_dir/guido.py
+++ Project/Prototype/guido_dir/guido.py
@@ -4,6 +4,8 @@ import dlib
 from pathlib import Path
 from imutils import face_utils
 import screeninfo
+import time
+from paz.pipelines import MiniXceptionFER
 
 
 
@@ -19,7 +21,18 @@ scale = 2
 
 # data from web cam
 cap = cv2.VideoCapture(0)
-while True:
+# we work with one person
+arr_of_mood = []
+max_length_arr_of_mood = 200
+# message of staying in vision
+message = 'please stay in my field of vision'
+message_length = len(message)
+
+
+classify = MiniXceptionFER()
+
+while len(arr_of_mood)<max_length_arr_of_mood:
+    time.sleep(0.02)
     # black background
     bg = cv2.imread("../add_data_dir/background_black.jpg")
     # Getting out image by webcam 
@@ -29,25 +42,45 @@ while True:
         
     # Get faces into webcam's image
     rects = detector(gray, 0)
-    
     # For each detected face, find the landmark.
     for (i, rect) in enumerate(rects):
         # Make the prediction and transfom it to numpy array
         shape = predictor(gray, rect)
         shape = face_utils.shape_to_np(shape)
 	    # create computer man 
-        
         man = Computer_Man(x0=x_parallax,y0 = y_parallax, scale=scale)
         man.draw_man(bg,shape)
-
+    cv2.putText(bg,message,(monitor_width - 700, 100),cv2.FONT_HERSHEY_SIMPLEX, 1,(255,255,255))
+    # success percentage
+     
+    current_status =  int(len(arr_of_mood)/max_length_arr_of_mood*10)
+    cv2.putText(bg,(">" + "="*current_status) + ">",(monitor_width - 700, 150),cv2.FONT_HERSHEY_SIMPLEX, 1,(255,0,0))   
+    # add mood into array
+    inference = classify(image)
+    arr_of_mood.append(inference['class_name'])
     # Show the image
     cv2.imshow("Output", bg)
-    
-
-
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
         break
+cv2.destroyAllWindows()
+
+# we found out mood
+
+# now may drop that to server../find mood and make the animation
+#thread 1 find mood
+#thread 2 
+color = 0
+while(color<255):
+    bg = cv2.imread("../add_data_dir/background_black.jpg")
+    cv2.putText(bg,"Some magic..",(200, 200),cv2.FONT_HERSHEY_SIMPLEX, 1,(color,color,color))   
+    color = color+1
+    cv2.imshow("Output",bg)
+    time.sleep(0.001)
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+        break
+
 
 cv2.destroyAllWindows()
 cap.release()
